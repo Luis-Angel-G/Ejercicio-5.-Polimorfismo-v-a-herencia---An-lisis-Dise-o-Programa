e2ee2984137e9193186ec8c6442d6476cdb2f604
Luis-Angel-G <langelgirona@gmail.com>
2024-10-05 19:28:21 -0600

    Se crea constructor y getters y setters

---
+++ Animal.java
@@ -0,0 +1,35 @@
+public abstract class Animal{
+    protected String nombrec;
+    protected String habitat;
+    protected double esperanza;
+    
+    public Animal(String nombrec, String habitat, double esperanza) {
+        this.nombrec = nombrec;
+        this.habitat = habitat;
+        this.esperanza = esperanza;
+    }
+
+    public String getNombrec() {
+        return nombrec;
+    }
+
+    public void setNombrec(String nombrec) {
+        this.nombrec = nombrec;
+    }
+
+    public String getHabitat() {
+        return habitat;
+    }
+
+    public void setHabitat(String habitat) {
+        this.habitat = habitat;
+    }
+
+    public double getEsperanza() {
+        return esperanza;
+    }
+
+    public void setEsperanza(double esperanza) {
+        this.esperanza = esperanza;
+    }
+}
