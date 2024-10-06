8b3d38017ef1b9e32694b3a820630701f96e6b10
Luis-Angel-G <langelgirona@gmail.com>
2024-10-06 12:57:07 -0600

    Se quita recinto de los atributos

--- Gestion.java
+++ Gestion.java
@@ -4,8 +4,8 @@ public class Gestion{
     private ArrayList<Animal> listaAnimales = new ArrayList<>();
 
     public String crearFelino(String nombrec, String habitat, double esperanza, int pelaje, int crias, double peso, double gestacion, boolean extincion,
-    int dieta, double costodieta, String especie, int recinto, int tamanio, double cola, String color, double velocidad){
-        Felino nuevoFelino = new Felino(nombrec, habitat, esperanza, pelaje, crias, peso, gestacion, extincion, dieta, costodieta, especie, recinto, tamanio, cola, color, velocidad);
+    int dieta, double costodieta, String especie, int tamanio, double cola, String color, double velocidad){
+        Felino nuevoFelino = new Felino(nombrec, habitat, esperanza, pelaje, crias, peso, gestacion, extincion, dieta, costodieta, especie, tamanio, cola, color, velocidad);
         return nuevoFelino.toString();
     }
 }
